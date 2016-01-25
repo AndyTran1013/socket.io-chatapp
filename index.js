69f074339394b06d76af350d05199ac5ea82c3fb
Andy Tran <a.tran1013@gmail.com>
2016-01-24 22:47:42 -0500

    fixed typo on emit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ io.on("connection", function(socket){
 
 	socket.on("chat message", function(msg){
 		console.log("message: " + msg);
-		socket.emit('chat message', msg);
+		io.emit('chat message', msg);
 	});
 
 });
